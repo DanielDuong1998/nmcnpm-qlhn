2a551a91fcf9789aac9ebdae6030b6dd23a40b9d
DanielDuong1998 <dvkhangnt@gmail.com>
2020-11-12 21:06:24 +0700

    test method post

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,11 +1,23 @@
 var express = require('express');
-var router = express.Router();
+const bodyParser = require('body-parser');
 
 const userModel = require('../models/user.model');
+const { urlencoded } = require('body-parser');
+
+
+var router = express.Router();
+const urlencodedParser = bodyParser.urlencoded();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/', urlencodedParser, (req, res) => {
+  const { body } = req;
+  console.log('body: ', body.email);
+  res.send('respond with a resource');
+  
 });
 
 module.exports = router;
