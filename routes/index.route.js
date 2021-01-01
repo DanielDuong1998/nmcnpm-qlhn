938d043b358b746d83f0e9f5a4c1957a951b2636
DESKTOP-A4UPJPS\tranm <tranminhthien1199@gmail.com>
2021-01-01 16:08:16 +0700

    add  message

--- routes/index.route.js
+++ routes/index.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    //add messeage
+    //add messeage  f
     res.render('index', { title: 'Express' });
 });
 
