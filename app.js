bb79d7f2555e56c9e9d1efa735b36fb903cb1e7a
DESKTOP-A4UPJPS\tranm <tranminhthien1199@gmail.com>
2021-01-05 10:57:25 +0700

    xay dung khung cua views. xay dung khung trang home, thanh nav

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
 const createError = require('http-errors');
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index.route');
+//const indexRouter = require('./routes/index.route');
 const usersRouter = require('./routes/user.route');
 const adminRouter = require('./routes/admin.route');
 const authRouter = require('./routes/auth.route');
 
 const app = express();
 
+app.use(express.urlencoded({
+    extended: true
+}));
+
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.engine('hbs', exphbs({
+    defaultLayout: 'main.hbs',
+    extname: '.hbs',
+    layoutsDir: 'views/layouts',
+    partialsDir: 'views/partials',
+    helpers: {
+        format(val) {
+            return numeral(val).format('0,0') + 'd';
+        }
+    }
+}));
+app.set('view engine', 'hbs');
+
+//app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,25 +43,47 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+
+
+//app.use('/', indexRouter);
 app.use('/user', usersRouter);
 app.use('/admin', adminRouter);
 app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function(req, res) {
+    res.render('404', {
+        layout: false
+    })
 });
+
+// app.use(function(req, res, next) {
+//     next(createError(404));
+// });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// default error handler
+app.use(function(err, req, res, next) {
+    console.error(err.stack)
+    res.render('500', {
+        layout: false
+    })
 });
 
+// app.use(function(err, req, res, next) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//     // render the error page
+//     res.status(err.status || 500);
+//     res.render('error');
+// });
+
+
+
 module.exports = app;
+// const PORT = 3000;
+// app.listen(PORT, _ => {
+//     console.log(`Example app listening at http://localhost:${PORT}`);
+// });
