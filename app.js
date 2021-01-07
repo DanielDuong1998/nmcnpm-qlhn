5cf8947e53afd20bd028af6fe89e9e29b429e291
DanielDuong1998 <dvkhangnt@gmail.com>
2021-01-07 09:22:10 +0700

    change branch

--- app.js
+++ app.js
@@ -28,11 +28,11 @@ app.engine('hbs', exphbs({
 }));
 app.set('view engine', 'hbs');
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render('home');
 });
 
-app.get('/index', function(req, res) {
+app.get('/index', function (req, res) {
     res.render('index', {
         layout: false
     });
@@ -72,7 +72,7 @@ app.use('/user', require('./routes/user.route'));
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res) {
+app.use(function (req, res) {
     res.render('404', {
         layout: false
     })
@@ -80,7 +80,7 @@ app.use(function(req, res) {
 
 // error handler
 // default error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     console.error(err.stack)
     res.render('500', {
         layout: false
