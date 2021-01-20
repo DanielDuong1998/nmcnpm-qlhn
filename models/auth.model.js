203cf33e51d0f82a6d714d81debfca3c6fe2a9da
DESKTOP-A4UPJPS\tranm <tranminhthien1199@gmail.com>
2021-01-20 17:04:10 +0700

    them view chi tiet hoi nghi

--- models/auth.model.js
+++ models/auth.model.js
@@ -1,48 +1,3 @@
-// // const bcrypt = require('bcryptjs');
-
-// const db = require('../utils/db');
-// // const userModel = require('../models/user.model');
-// // const adminModel = require('../models/admin.model');
-
-
-
-// const TBL_USERS = 'user';
-
-// module.exports = {
-//     all() {
-//         return db.load(`select * from ${TBL_USERS}`);
-//     },
-
-
-//     async single(id) {
-//         const rows = await db.load(`select * from ${TBL_USERS} where id = ${id}`);
-//         if (rows.length === 0)
-//             return null;
-//         return rows[0];
-//     },
-
-
-//     async singleByUserName(user_name) {
-//         const rows = await db.load(`select * from ${TBL_USERS} where user_name = '${user_name}'`);
-//         if (rows.length === 0)
-//             return null;
-//         return rows[0];
-//     },
-
-
-//     add(entity) {
-//         return db.add(entity, TBL_USERS);
-//     },
-
-
-// };
-
-
-
-
-
-
-
 
 
 const bcrypt = require('bcryptjs');
@@ -66,7 +21,6 @@ module.exports = {
         if (bcrypt.compareSync(entity.password, passwordHash)) {
             return row[0];
         }
-
         return null;
     },
 
